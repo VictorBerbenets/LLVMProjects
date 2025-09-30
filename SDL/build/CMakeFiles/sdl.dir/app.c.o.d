b4851be4f9a90c9d2086573446af7585e604d1cc
Berbenets Viktor <v.berbenets@syntacore.com>
2025-09-30 22:44:16 +0300

    deleted unused stuff

--- SDL/build/CMakeFiles/sdl.dir/app.c.o.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles/sdl.dir/app.c.o: /home/viktor/Desktop/LLVMProjects/SDL/app.c \
-  /home/viktor/Desktop/LLVMProjects/SDL/sim.h
--- SDL/build/CMakeFiles/sdl.dir/main.c.o.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles/sdl.dir/main.c.o: /home/viktor/Desktop/LLVMProjects/SDL/main.c \
-  /home/viktor/Desktop/LLVMProjects/SDL/sim.h
